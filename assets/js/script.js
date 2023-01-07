dc197a4b34cda4ba497ffd77ecec133d63f7c52e
h-bails <hollyannebailey@gmail.com>
2023-01-07 16:25:13 +0000

    Add and style instructions pop-up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-/** Toggles the music on and off on click */
+/** Toggles the music on and off when "Toggle Music" link is clicked */
 
 document.getElementById("toggle-music").addEventListener("click", playMusic)
 let audio = new Audio("https://incompetech.com/music/royalty-free/mp3-royaltyfree/Disco%20con%20Tutti.mp3");
@@ -12,3 +12,9 @@ function playMusic() {
         audio.pause();
     }
 }
+
+/** Displays instructions when 'Instructions' link is clicked */
+
+document.getElementById("view-instructions").addEventListener("click", showInstructions)
+
+
