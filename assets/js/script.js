05e3b36cb08b890bbf7498b5756d0b9ee7e54576
h-bails <hollyannebailey@gmail.com>
2023-01-07 18:17:29 +0000

    Add startGame function to fix display of prawns

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@
 
 document.getElementById("toggle-music").addEventListener("click", playMusic)
 let audio = new Audio("https://incompetech.com/music/royalty-free/mp3-royaltyfree/Disco%20con%20Tutti.mp3");
+let score = parseInt(document.getElementById("score").innerText)
+let missed = parseInt(document.getElementById("missed").innerText)
 
 function playMusic() {
     if (audio.paused) {
@@ -37,17 +39,44 @@ function closePopUp () {
 
 /** Displays image of prawn in a random div */
 
-document.getElementById("start-game").addEventListener("click", displayPrawns)
+document.getElementById("start-game").addEventListener("click", startGame)
+
+function startGame() {
+    displayPrawns();
+    setInterval(displayPrawns, 3000)
+}
 
 function displayPrawns () {
     let holes = document.getElementsByClassName("mole-area");
     let randomIndex = Math.floor(Math.random() * holes.length);
     let randomDiv = holes[randomIndex]
-    randomDiv.innerHTML = `<img src="./assets/images/prawn-hole.png">`
+    let prawn = document.createElement("img")
+    prawn.src = "./assets/images/prawn-hole.png"
 
+    // Add a prawn to a random div
+    randomDiv.innerHTML = ""
+    randomDiv.appendChild(prawn);
+
+    // Increase the score when prawn is clicked
+    prawn.addEventListener("click", function(){
+        console.log("Prawn whacked!");
+        ++score;
+        document.getElementById("score").innerText = score;
+    })
+
+    // Revert the random div to an empty hole
     setTimeout(function() {
         randomDiv.innerHTML = `<img src="./assets/images/empty-hole.png" alt="empty mole hole">`;
       }, 2000);
+      
 }
 
-setInterval(displayPrawns, 3000);
+/** Increase difficulty as score increases */
+
+if (score > 5) {
+    setInterval(displayPrawns, 2000);
+} else if (score > 10) {
+    setInterval(displayPrawns, 1000)
+} else if (score > 15) {
+    setInterval(displayPrawns, 500)
+}
